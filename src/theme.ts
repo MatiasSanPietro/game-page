942a8d2d2d9b27f8d6735795019e7209d7407b92
MatiasSanPietro <matisanpietro98@gmail.com>
2023-08-16 17:12:52 -0300

    heading

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
   initialColorMode: "dark",
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  // colors: {
+  //   gray: {
+  //     50: "#faf5ff",
+  //     100: "#e9d8fd",
+  //     200: "#d6bcfa",
+  //     300: "#b794f4",
+  //     400: "#9f7aea",
+  //     500: "#805ad5",
+  //     600: "#6b46c1",
+  //     700: "#553c9a",
+  //     800: "#44337a",
+  //     900: "#322659",
+  //   },
+  // },
+});
 
 export default theme;
